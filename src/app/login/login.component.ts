e7a6e9e90417f970a71d8340e4e951ceee8cb479
milan <milankumarbth1999@gmail.com>
2023-02-20 00:44:30 +0530

    update login user id

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -19,7 +19,7 @@ export class LoginComponent implements OnInit {
   login() {
     // alert('hey')
     // if (this.user === "mailto:admin@uxdlab.us" && this.pass === "Admin@123") {
-    if (this.user === 'admin@uxdlab.us' && this.pass === 'admin@123') {
+    if (this.user === 'admin@milan.com' && this.pass === 'admin@123') {
       // this.router.navigate(['/dashboard']);
       this.router.navigateByUrl('dashboard');
     }
